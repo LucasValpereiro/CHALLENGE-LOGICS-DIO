83f744cbe1854e150314ef5b6285288a6f14ed65
LucasValpereiro <lucasvalpereiro@hotmail.com>
2025-10-27 17:42:26 -0300

    added another variable and loops to show different ranks, xp's and heroes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-let nomeHerois = [];
-let xpHerois = [];
+// Variáveis para armazenar nomes e XP dos heróis
+let nomeHerois = ["Homem de Ferro", "Capitão América", "Thor", "Hulk", "Viúva Negra", "Gavião Arqueiro"];
+let xpHerois = ["1000", "2500", "6000", "8000", "9500", "11000"];
+let rankHerois = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
 
+// Adicionando nomes e XP dos heróis
 if (xpHerois < 1000){
     console.log("Ferro");
 } else if (xpHerois > 1000 && xpHerois <= 2000){
@@ -19,3 +22,10 @@ if (xpHerois < 1000){
     console.log("Radiante");
 }
 
+// Loop para exibir os Nomes, XP e nível dos heróis
+for (let i = 0; i < nomeHerois.length; i++) {
+    console.log("O Herói de nome " + nomeHerois[i] + " tem XP de " + xpHerois[i] + " e está no rank " + rankHerois[i] + ".");
+}
+
+// Saída dos nomes e XP dos heróis
+console.log("O Herói de nome ${nomeHerois} tem XP de ${xpHerois} e está no rank ${rankHerois}.");
