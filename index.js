9cdf5f26d4adbdc70ca8e638c488799eb6809af7
LucasValpereiro <lucasvalpereiro@hotmail.com>
2025-10-29 12:14:23 -0300

    att formatting doc

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 // Variáveis para armazenar nomes e XP dos heróis
-let nomeHerois = ["Homem de Ferro", "Capitão América", "Thor", "Hulk", "Viúva Negra", "Gavião Arqueiro", "Pantera Negra", "Doutor Estranho"];
+let nomeHerois = [
+  "Homem de Ferro",
+  "Capitão América",
+  "Thor",
+  "Hulk",
+  "Viúva Negra",
+  "Gavião Arqueiro",
+  "Pantera Negra",
+  "Doutor Estranho",
+];
 let xpHerois = [1000, 1500, 2500, 6000, 7500, 9000, 10000, 12000];
 
 // Loop para exibir os Nomes, XP e Níveis dos heróis
 for (let i = 0; i < nomeHerois.length; i++) {
-    let rankHerois;
+  let rankHerois;
 
-// Adicionando nomes e XP dos heróis
-if (xpHerois[i] <= 1000){
-    rankHerois = "Ferro";   
-} else if (xpHerois[i] >= 1001 && xpHerois[i] <= 2000){
+  // Adicionando nomes e XP dos heróis
+  if (xpHerois[i] <= 1000) {
+    rankHerois = "Ferro";
+  } else if (xpHerois[i] >= 1001 && xpHerois[i] <= 2000) {
     rankHerois = "Bronze";
-} else if (xpHerois[i] >= 2001 && xpHerois[i] <= 5000){
+  } else if (xpHerois[i] >= 2001 && xpHerois[i] <= 5000) {
     rankHerois = "Prata";
-} else if (xpHerois[i] >= 5001 && xpHerois[i] <= 7000){
+  } else if (xpHerois[i] >= 5001 && xpHerois[i] <= 7000) {
     rankHerois = "Ouro";
-} else if (xpHerois[i] >= 7001 && xpHerois[i] <= 8000){
+  } else if (xpHerois[i] >= 7001 && xpHerois[i] <= 8000) {
     rankHerois = "Platina";
-} else if (xpHerois[i] >= 8001 && xpHerois[i] <= 9000){
+  } else if (xpHerois[i] >= 8001 && xpHerois[i] <= 9000) {
     rankHerois = "Ascendente";
-} else if (xpHerois[i] >= 9001 && xpHerois[i] <= 10000){
+  } else if (xpHerois[i] >= 9001 && xpHerois[i] <= 10000) {
     rankHerois = "Imortal";
-} else {
+  } else {
     rankHerois = "Radiante";
-}
+  }
 
-console.log(`O herói ${nomeHerois[i]} possui ${xpHerois[i]} de XP e é do rank ${rankHerois}.`);
+  console.log(
+    `O herói ${nomeHerois[i]} possui ${xpHerois[i]} de XP e é do rank ${rankHerois}.`
+  );
 }
